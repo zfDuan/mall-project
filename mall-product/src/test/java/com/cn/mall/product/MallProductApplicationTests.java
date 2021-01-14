816a61f04489d921f4ddba0678fe0c23d82c2716
duanzhf <925043897@qq.com>
2021-01-14 22:29:52 +0800

    product

--- mall-product/src/test/java/com/cn/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/cn/mall/product/MallProductApplicationTests.java
@@ -24,8 +24,8 @@ public class MallProductApplicationTests {
  /*       // Endpoint以成都为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-chengdu.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "LTAI4G3TGTrAzi8pgBQkKWAa";
-        String accessKeySecret = "rEMMUmysccySdTRyWpWikjasb1SPQ0";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         String bucketName = "mall-project-dzf";
         // <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
         String objectName = "C:\\Users\\duanzhf\\Desktop\\test.jfif";
